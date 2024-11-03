7c6b3bb57d714e5a89d71963c038863fccce320f
yashweblife <yashweblife20@gmail.com>
2024-11-03 08:11:08 -0800

    new code

--- go/AA_Fasta_Seq_Filter/AA_Fasta_Seq_Filter.go
+++ go/AA_Fasta_Seq_Filter/AA_Fasta_Seq_Filter.go
@@ -3,32 +3,39 @@ package aafastaseqfilter
 // import "github.com/yashweblife/BioSphere/go/FastaPoint"
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	FastaPoint "github.com/yashweblife/BioSphere/go/FastaPoint"
 )
 
-func GetDataFromFile(filename string) []FastaPoint.FastaPoint {
-	data, err := ioutil.ReadFile(filename)
+func GetDataFromFile(filename string) ([]FastaPoint.FastaPoint, error) {
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println(err)
+		return nil, err
 	}
 	initial_split := strings.Split(string(data), ">")
+	if initial_split[0] == "" {
+		initial_split = initial_split[1:]
+	}
 	fasta_points := []FastaPoint.FastaPoint{}
-
-	for i := 1; i < len(initial_split); i++ {
+	for i := 0; i < len(initial_split); i++ {
 		val := strings.Index(initial_split[i], "\n")
 		if val != -1 {
-			fasta_points = append(fasta_points, FastaPoint.NewFastaPoint(initial_split[i][:val], initial_split[i][val+1:]))
+			header := initial_split[i][:val]
+			sequence := initial_split[i][val+1:]
+			fasta_points = append(fasta_points, FastaPoint.NewFastaPoint(header, sequence))
 		}
 	}
-	return fasta_points
+	return fasta_points, nil
 }
 
-func WriteDataToFile(data string, filename string) {
-	os.WriteFile(filename, []byte(data), 0644)
+func WriteDataToFile(data string, filename string) error {
+	err := os.WriteFile(filename, []byte(data), 0644)
+	return err
 }
 
 func GetSimilarityScore(seq1 string, seq2 string) int {
@@ -44,8 +51,12 @@ func GetSimilarityScore(seq1 string, seq2 string) int {
 	return score
 }
 
-func SequenceFilter(filename string, PERCENTAGE float64) {
-	fasta_points := GetDataFromFile(filename)
+func SequenceFilter(filename string, PERCENTAGE float64) error {
+	fasta_points, err := GetDataFromFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	for i := 0; i < len(fasta_points); i++ {
 		test_point := fasta_points[i]
 		for j := 0; j < len(fasta_points); j++ {
@@ -53,7 +64,12 @@ func SequenceFilter(filename string, PERCENTAGE float64) {
 				continue
 			}
 			th := int(PERCENTAGE * float64(len(test_point.Sequence)))
-			if GetSimilarityScore(test_point.Sequence, fasta_points[j].Sequence) >= th {
+			score := GetSimilarityScore(test_point.Sequence, fasta_points[j].Sequence)
+			fmt.Println("Comapring", test_point.Header)
+			fmt.Println("Score:", score, "/", th)
+
+			if score >= th {
+				fmt.Println("Removing", fasta_points[j].Header)
 				fasta_points = append(fasta_points[:j], fasta_points[j+1:]...)
 				j--
 			}
@@ -63,5 +79,6 @@ func SequenceFilter(filename string, PERCENTAGE float64) {
 	for i := 0; i < len(fasta_points); i++ {
 		output += fasta_points[i].ToString() + "\n"
 	}
-	WriteDataToFile(output, filename)
+	err = WriteDataToFile(output, "output"+strconv.FormatInt(time.Now().Unix(), 10)+".fasta")
+	return err
 }
--- go/AA_Fasta_Seq_Filter/AA_Fasta_Seq_Filter_test.go
+++ go/AA_Fasta_Seq_Filter/AA_Fasta_Seq_Filter_test.go
@@ -1,1 +1,21 @@
 package aafastaseqfilter
+
+import (
+	"testing"
+)
+
+func TestSequenceFilter(t *testing.T) {
+	err := SequenceFilter("test_data/test_data_align.fas", 0.95)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("Test passed")
+}
+
+func BenchmarkSequenceFilter(b *testing.B) {
+	err := SequenceFilter("test_data/test_data_align.fas", 0.95)
+	if err != nil {
+		b.Error(err)
+	}
+	b.Log("Test passed")
+}
