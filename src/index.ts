c4b1385b4512fd41f419ef1131bd536b4a88fe5c
yashweblife <yashweblife20@gmail.com>
2024-11-03 19:25:24 -0800

    add new test function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { SequenceFilter } from "./Projects/AA_Fasta_Seq_Filter";
-
-SequenceFilter("test_data_align.fas");
+import { SequenceFilter1 } from "./Projects/AA_Fasta_Seq_Filter";
+const FILE_NAME = "H5_PB2_all_212_Copy.fasta"
+// SequenceFilter(FILE_NAME);
+// console.log("\n\nOTHER\n\n");
+SequenceFilter1(FILE_NAME);
